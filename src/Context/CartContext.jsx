4c0464dd1802b50c94a2263de8d0633f399aeb6c
mahlet-1 <mahichanie126@gmail.com>
2026-08-29 18:33:47 +0300

    Add a local storage feature for cart component

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,15 +1,25 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useReducer, useEffect } from 'react';
 import { cartReducer, initialState } from './cartReducer';
 
 export const CartContext = createContext();
+
 export function CartProvider({ children }) {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
+  const [state, dispatch] = useReducer(cartReducer, initialState, () => {
+    const localData = localStorage.getItem("shop_cart");
+    return localData ? { cart: JSON.parse(localData) } : initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("shop_cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
-    <CartContext.Provider value={{ cart: state.cart , dispatch }}>
+    <CartContext.Provider value={{ cart: state.cart, dispatch }}>
       {children}
     </CartContext.Provider>
   );
 }
 
-
+export function useCart() {
+  return useContext(CartContext);
+}
